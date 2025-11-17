3b668320f589b3d1b0fd3576d72f13516357d5d0
kakurega-labo <udon05.0025@gmail.com>
2025-11-18 02:07:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -259,7 +259,7 @@ const cartTemplate = (product) => `
                     </button>
                     
                     <a href="#" class="back-to-shop back-btn-bottom">
-                        <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="vertical-align: middle; margin-right: 5px;">
+                        <svg width="1em" height="1em" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="vertical-align: middle; filter: invert(1);">
                             <path d="M15 18L9 12L15 6" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                         </svg>
                         ショップに戻る
